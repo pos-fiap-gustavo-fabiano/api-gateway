b04e9005c0a2036b5819ebc89b958f6e64ee807b
Fabiano Teixeira <fabianotxss@gmail.com>
2025-03-16 15:17:25 -0300

    ajutes rotas

--- ApiGateway/Program.cs
+++ ApiGateway/Program.cs
@@ -7,19 +7,33 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 builder.Services.AddControllers();
-builder.Configuration.AddJsonFile("ocelot.json", optional: true, reloadOnChange: true);
+var configBuilder = new ConfigurationBuilder();
+configBuilder.AddEnvironmentVariables();
+var configuration = configBuilder.Build();
+
+// Obtenha o conteúdo do arquivo ocelot.json
+string ocelotJson = File.ReadAllText("ocelot.json");
+
+// Substitua os placeholders pelas variáveis de ambiente
+var apiHost = Environment.GetEnvironmentVariable("API_CONTACT_HOST") ?? "localhost";
+ocelotJson = ocelotJson.Replace("${API_CONTACT_HOST}", apiHost);
+
+var port = Environment.GetEnvironmentVariable("PORT") ?? "7117";
+ocelotJson = ocelotJson.Replace("${PORT}", port);
+
+// Salve o arquivo com as substituições
+File.WriteAllText("ocelot.temp.json", ocelotJson);
+builder.Configuration.AddJsonFile("ocelot.temp.json", optional: false, reloadOnChange: true);
+
 builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+app.UseSwagger();
+app.UseSwaggerUI();
+
 
 app.UseAuthorization();
 
