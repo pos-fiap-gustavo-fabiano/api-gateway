25a5ef852634686fe06d77fc772a1870c62f10e8
Fabiano Teixeira <fabianotxss@gmail.com>
2025-03-16 14:44:56 -0300

    initial commit

---
+++ ApiGateway/Program.cs
@@ -0,0 +1,29 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+builder.Services.AddControllers();
+builder.Configuration.AddJsonFile("ocelot.json", optional: true, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseAuthorization();
+
+app.MapControllers();
+await app.UseOcelot();
+
+app.Run();
